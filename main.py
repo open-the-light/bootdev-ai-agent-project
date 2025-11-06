f80b2beeb2d2233b5f6205b7c039d760521cfdaa
Alex Eckersall <a.j.eckersall@gmail.com>
2025-11-06 15:19:36 +0900

    added command line entry

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import os
+import sys
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+from google import genai
+from google.genai import types
 
 def main():
-    print("Hello from bootdev-ui-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    messages = []
+
+    try:
+        user_prompt = sys.argv[1]
+    except Exception as e:
+        print("Enter your query as a command line argument please...")
+        sys.exit(1)
+    response = client.models.generate_content(model="gemini-2.0-flash-001",
+                                              contents=user_prompt)
+
+    usage = response.usage_metadata
+
+    messages.append(types.Content(role="user", parts=[types.Part(text=user_prompt)]))
+    print(response.text)
+    print(f"Prompt tokens: {usage.prompt_token_count}\nResponse tokens: {usage.candidates_token_count}") # type: ignore
 
 
 if __name__ == "__main__":
